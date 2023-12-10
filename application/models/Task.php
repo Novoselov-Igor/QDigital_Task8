97d56060741e01bc9d6b335f2049f0703e83f876
Novoselov-Igor <barszx23@gmail.com>
2023-12-11 00:00:01 +0400

    Modified models

--- application/models/Task.php
+++ application/models/Task.php
@@ -6,5 +6,67 @@ use application\core\Model;
 
 class Task extends Model
 {
+    public $table = 'tasks';
 
+    public $fillable = [
+        'user_id',
+        'description',
+        'status'
+    ];
+
+    protected function insert()
+    {
+        $sql = 'INSERT INTO ' . $this->table . '(user_id, description, status) VALUES (:user_id, :description, :status)';
+        $this->db->query($sql, $this->fillable);
+    }
+
+    public function create($data)
+    {
+        if (!empty($data)) {
+            $this->fillable = $data;
+            $this->insert();
+        } else {
+            die('Передан пустой массив');
+        }
+    }
+
+    public function deleteTask($taskId, $userId)
+    {
+        $sql = 'DELETE FROM ' . $this->table . ' WHERE user_id = :userId AND id = :id';
+        $this->db->query($sql, ['id' => $taskId, 'userId' => (int)$userId]);
+    }
+
+    public function deleteTasksAll($userId)
+    {
+        $sql = 'DELETE FROM ' . $this->table . ' WHERE user_id = :userId';
+        $this->db->query($sql, ['userId' => (int)$userId]);
+    }
+
+    public function getStatus($taskId, $userId)
+    {
+        $sql = 'SELECT * FROM ' . $this->table . ' WHERE user_id = :userId AND id = :id';
+        return $this->db->fetch($sql, ['id' => $taskId, 'userId' => (int)$userId]);
+    }
+
+    public function readyUnreadyTask($taskId, $userId, $status)
+    {
+        $sql = 'UPDATE ' . $this->table . ' SET status = ' . $status . ' WHERE user_id = :userId AND id = :id';
+        $this->db->query($sql, ['id' => $taskId, 'userId' => (int)$userId]);
+    }
+
+    public function readyTasksAll($userId)
+    {
+        $sql = 'UPDATE ' . $this->table . ' SET status = 1 WHERE user_id = :userId';
+        $this->db->query($sql, ['userId' => (int)$userId]);
+    }
+
+    public function getTask($id)
+    {
+        if (isset($id)) {
+            $sgl = 'SELECT * FROM ' . $this->table . ' WHERE `user_id` = :id';
+
+            return $this->db->row($sgl, ['id' => $_COOKIE['userId']]);
+        }
+        return '';
+    }
 }
--- application/models/User.php
+++ application/models/User.php
@@ -6,5 +6,63 @@ use application\core\Model;
 
 class User extends Model
 {
+    protected $table = 'users';
+    protected $fillable = [
+        'login',
+        'password'
+    ];
 
+    private function hasAccount()
+    {
+        $sql = 'SELECT * FROM ' . $this->table . ' WHERE `login` = :login';
+        return $this->db->query($sql, ['login' => $this->fillable['login']]);
+    }
+
+    private function authenticate()
+    {
+        $user = $this->hasAccount();
+        $user = $user->fetch(\PDO::FETCH_ASSOC);
+        setcookie('userId', $user['id'], time() + 360000, '/');
+    }
+
+    private function checkUser()
+    {
+        $check = $this->hasAccount();
+        if ($check->rowCount() <= 0) {
+            $this->insert();
+        }
+        $this->authenticate();
+    }
+
+    protected function insert()
+    {
+        $sql = 'INSERT INTO ' . $this->table . '(login, password) VALUES (:login, :password)';
+        $this->db->query($sql, $this->fillable);
+    }
+
+    public function create($data)
+    {
+        if (!empty($data)) {
+            $this->fillable = $data;
+            $this->checkUser();
+        } else {
+            die('Передан пустой массив');
+        }
+    }
+
+    public function getUser()
+    {
+        if (isset($_COOKIE['userId'])) {
+            $sgl = 'SELECT * FROM ' . $this->table . ' WHERE `id` = :id';
+
+            $user = $this->db->query($sgl, ['id' => $_COOKIE['userId']]);
+            return $user->fetch(\PDO::FETCH_ASSOC);
+        }
+        return '';
+    }
+
+    public static function isAuthorized()
+    {
+        return !!($_COOKIE['userId'] ?? false);
+    }
 }
