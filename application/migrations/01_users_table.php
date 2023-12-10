e53c9d9d2c80dafb68c645b6b61add29c944fae3
Novoselov-Igor <barszx23@gmail.com>
2023-12-10 23:59:33 +0400

    Modified migrations

--- application/migrations/01_users_table.php
+++ application/migrations/01_users_table.php
@@ -1,9 +1,21 @@
 <?php
-$pdo = require_once '../config/db.php';
 
-$pdo->exec('CREATE table IF NOT EXISTS users (
+$config = require '../config/db.php';
+
+$dsn = "mysql:host=$config[host]; dbname=$config[name];charset=$config[charset]";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+$pdo = new PDO($dsn, $config['user'], $config['password'], $opt);
+
+$sql = 'CREATE table IF NOT EXISTS users (
     id INT PRIMARY KEY  AUTO_INCREMENT,
     login VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-    ');
+    ';
+
+$pdo->exec($sql);
